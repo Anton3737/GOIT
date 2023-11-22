cc8658f6426a252b3c077856bb589d672ef1c45d
AntonBar <BarashASUA@gmail.com>
2023-11-22 21:44:12 +0200

    Task 12 виконано

--- src/main/java/task11/ex5/StreamZipper.java
+++ src/main/java/task11/ex5/StreamZipper.java
@@ -1,67 +1,51 @@
 package task11.ex5;
 
 import java.util.Iterator;
+import java.util.Spliterators;
 import java.util.stream.Stream;
 import java.util.stream.StreamSupport;
 
 public class StreamZipper {
 
-    public static <T> Stream<T> zip(Stream<T> first, Stream<T> second) {
-        ZipperIterator<T> iterator = new ZipperIterator<>(first.iterator(), second.iterator());
-        Iterable<T> iterable = () -> iterator;
+    private static <T> Stream<T> zip(Stream<T> first, Stream<T> second) {
+        Iterator<T> iterator1 = first.iterator();
+        Iterator<T> iterator2 = second.iterator();
+        Iterator<T> zippingIterator = new Iterator<T>() {
+            private boolean swicher = true;
 
-        return StreamSupport.stream(iterable.spliterator(), false)
-                .onClose(() -> {
-                    closeSilently(first);
-                    closeSilently(second);
-                });
-    }
-
-    private static void closeSilently(Stream<?> stream) {
-        try {
-            stream.close();
-        } catch (Exception ignored) {
-            // Ignore exceptions on close
-        }
-    }
-
-    private static class ZipperIterator<T> implements Iterator<T> {
-        private final Iterator<T> iteratorFirst;
-        private final Iterator<T> iteratorSecond;
-
-        private boolean useFirst = true;
-
-        public ZipperIterator(Iterator<T> iteratorFirst, Iterator<T> iteratorSecond) {
-            this.iteratorFirst = iteratorFirst;
-            this.iteratorSecond = iteratorSecond;
-        }
-
-        @Override
-        public boolean hasNext() {
-            return iteratorFirst.hasNext() || iteratorSecond.hasNext();
-        }
-
-        @Override
-        public T next() {
-            if (!hasNext()) {
-                throw new IllegalStateException("No more elements");
+            @Override
+            public boolean hasNext() {
+                if (swicher) {
+                    return iterator1.hasNext();
+                } else {
+                    return iterator2.hasNext();
+                }
             }
-            T next;
-            if (useFirst) {
-                next = iteratorFirst.next();
-            } else {
-                next = iteratorSecond.next();
+
+            @Override
+            public T next() {
+                T value;
+                if (swicher) {
+                    value = iterator1.next();
+                } else {
+                    value = iterator2.next();
+                }
+                swicher = !swicher;
+                return value;
             }
-            useFirst = !useFirst;
-            return next;
-        }
+        };
+
+        return StreamSupport.stream(
+                Spliterators.spliteratorUnknownSize(zippingIterator, 0),
+                false
+        );
     }
 
     public static void main(String[] args) {
         Stream<Integer> stream1 = Stream.of(1, 2, 3, 4);
         Stream<Integer> stream2 = Stream.of(5, 6, 7, 8, 9);
 
-        Stream<Integer> zippedStream = zip(stream1, stream2);
+        Stream<Integer> zippedStream = zip(stream1, stream2).sorted();
 
         zippedStream.forEach(System.out::println);
     }
